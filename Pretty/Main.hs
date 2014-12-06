f561e6fe675409c978b7229fbce45450e10f6c40
Nicolas <nicolas.rolland@gmail.com>
2014-12-06 17:18:45 +0100

    trying stuff

--- Pretty/Main.hs
+++ Pretty/Main.hs
@@ -6,14 +6,15 @@ import Prelude
 data Tree  = Node String [Tree]
              deriving(Show)
 
-showTree (Node s ts) =  group ( text s <> nest (length s) (showBracket ts) )
-showBracket []       =  nil
-showBracket ts       =  text "[" <> nest 1 (showTrees ts) <> text "]"
+showTree (Node s ts) =  group ( text s <> (showBracket' ts) )
+showBracket' []       =  nil
+showBracket' ts       =  text "[" <> nest 1 (line <> showTrees ts) <> line <> text "]"
 showTrees [t]        =  showTree t
 showTrees (t:ts)     =  showTree t <> text "," <> line <> showTrees ts
 
 
-a =  Node  "aaa"  [Node "bbbbb" [Node "ccc" [], Node "dd" []], Node "eee" [], Node "fff" [Node "gg" [], Node "hhh" [], Node "ii" []]]
+a =  Node  "aaa"  [Node "bbbbb" [Node "ccc" [], Node "dd" []], Node "eee" [], 
+                        Node "fff" [Node "gg" [], Node "hhh" [], Node "ii" []]]
 
 b =  Node  "aaa"  [Node "bbbbb" [], Node "ccc" []]
 
@@ -24,4 +25,4 @@ adoc = showTree a
 main :: IO ()
 main = 
    -- print $ layout adoc
-   putStrLn $ pretty 5 adoc
+   putStrLn $ pretty 20 adoc
