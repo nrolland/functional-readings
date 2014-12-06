3c0364f296a046de1020a35494c10a2e4d0334ea
Nicolas <nicolas.rolland@gmail.com>
2014-12-06 16:34:13 +0100

    completing <> and nest for the added case

--- Pretty/Pretty.hs
+++ Pretty/Pretty.hs
@@ -33,21 +33,21 @@ line   = 0 `Line` Nil
 (s `Text` x) <> y   = s `Text` (x <> y)  --appending a doc y when we already have s text  
 (i `Line` x) <> y =   i `Line` (x <> y)  -- same when we have a line, we just append to the end 
 Nil <> y = y                             -- appending to Nil is the appendee itself
-
+(x `Union` y) <> z = (x <> z) `Union` (y <> z) -- we need to define <> on the new case 
 
 nest i (s `Text` x)    = s `Text` nest i x -- s has no notion of line, so we just pass the job
 nest i (j `Line` x)  =  (i + j) `Line` x   -- nest acts on Lines by shifting the block
 nest i Nil  = Nil                          -- and does nothing to empty Doc
-
+nest i (x `Union` y) = nest i x `Union` nest i y  -- we need to define nest on the new case 
 
 (<|>) x y = x `Union` y    --our representation allows us to map directly to the constructor
 
 group x      = flatten x <|> x  -- by the semantic of group we can express in term of abstract ops
 
-flatten Nil = undefined
-flatten (i `Line` x)  =  undefined
-flatten (s `Text` x)  = undefined
-flatten (x `Union` y)  = undefined
+flatten Nil           = Nil
+flatten (i `Line` x)  = " " `Text` flatten x --flatten remove line return and their begin spaces
+flatten (s `Text` x)  = s `Text` flatten x  -- we dont touch Text which represent 'pure' text 
+flatten (x `Union` y) = flatten x  -- this invariant has to be respected
 
 
 pretty i x = undefined
