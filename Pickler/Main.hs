ea8eb883f819bb7360cdf26dc21ca40a7ada33f7
Nicolas Rolland <nicolas.rolland@gmail.com>
2014-12-12 08:04:35 +0000

    error to spot

--- Pickler/Main.hs
+++ Pickler/Main.hs
@@ -5,21 +5,30 @@ import Prelude
 data URL = URL { protocol :: String,
                  host :: String,
                  port :: Maybe Int,
-                 file :: String}
+                 file :: String} deriving(Show)
 url = wrap (\x -> (protocol x, host x, port x, file x),
             \(pr,h,pt,f) -> URL {protocol = pr, host=h, port = pt, file = f} )
             (quad string string (pMaybe nat) string)
             
-data Bookmark = Link (String, URL)  | Folder (String, [Bookmark])
-bookmark = alt tag [undefined,
-                     undefined]
+data Bookmark = Link (String, URL)  | Folder (String, [Bookmark]) deriving(Show)
+bookmark =
+    alt tag [ wrap (\(Link(s,u)) -> (s,u), Link )
+                       (pair string url) ,
+              wrap (\(Folder(s,bs)) -> (s,bs), Folder)
+                    (pair string (list bookmark)) ]
            where tag (Link _) = 0; tag (Folder _) = 1
 
+type Bookmarks = [Bookmark] 
+bookmarks = list bookmark
+            
 x = [ Link("Andrew", URL {protocol = "http",
                           host = "research.microsoft.com",
                           port = Nothing,
                           file = "users/akenn" })]
+px = pickle bookmarks x
+x' = unpickle bookmarks px
               
 main                    :: IO ()
-main                    =  do c <- getChar
-                              putChar c
+main                    =  do putStr px
+                              putStr "Hi"
+                              putStr $ show x'
