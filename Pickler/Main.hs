87241f3295923680b1cc7211a6f9f2725e9741fe
Nicolas Rolland <nicolas.rolland@gmail.com>
2014-12-12 07:41:22 +0000

    bookmark datatype

--- Pickler/Main.hs
+++ Pickler/Main.hs
@@ -1,5 +1,18 @@
 import System.Environment
+import Pickler.Pickle
+import Prelude
 
+data URL = URL { protocol :: String,
+                 host :: String,
+                 port :: Maybe Int,
+                 file :: String}
+data Bookmark = Link (String, URL)  | Folder (String, [Bookmark])
+
+x = [ Link("Andrew", URL {protocol = "http",
+                          host = "research.microsoft.com",
+                          port = Nothing,
+                          file = "users/akenn" })]
+              
 main                    :: IO ()
 main                    =  do c <- getChar
                               putChar c
